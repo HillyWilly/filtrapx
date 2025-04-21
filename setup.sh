70b78400ade841d9ec80c64d5291a77c3e2cbad0
Hildebrando <hildebrando.24104708360041@faeterj-rio.edu.br>
2025-04-20 21:08:48 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,4 +6,7 @@ pip install -r requirements.txt
 echo "Dependências instaladas com sucesso."
 
 # Apagar o script após execução
+
+echo "Execute o código desta maneira 'filtrapx arquivo.txt --nome JOAO --sexo M --idade_min 25 --idade_max 40' "
+
 rm -- "$0"
